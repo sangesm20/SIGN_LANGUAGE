03ad4c44975119d78c61eca2c47054514ef29735
sangesm20 <sangesm20@gmail.com>
2025-10-21 14:15:14 +0530

    first commit

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ detector = HandDetector(maxHands=1)
 classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
 offset = 20
 imgSize = 300
-folder = "Data/A"
+folder = "Data/C"
 counter = 0
 labels = ["A", "B", "C"]
 while True:
